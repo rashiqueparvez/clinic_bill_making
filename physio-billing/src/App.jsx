32caad9c734678bece753cb37867e661be2ea960
Rashique Parvez <61083544+rashiqueparvez@users.noreply.github.com>
2026-07-15 19:59:43 +0530

    Import NotFound component in App.jsx

--- physio-billing/src/App.jsx
+++ physio-billing/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-
+import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Billing from "./pages/Billing";
